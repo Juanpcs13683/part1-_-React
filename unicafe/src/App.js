c7583e9caff9252eead4f9d69f1d211cc064e8e1
Juan Cañon <84336310+Juanpcs13683@users.noreply.github.com>
2022-09-25 15:14:29 +1000

    Unicafe Step 2
    step 2 completed created components of all, average and positive

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-const H1 = (props) => (
+const Title1 = (props) => (
   <h1>{props.text}</h1>
 )
 
-const H2 = (props) => (
+const Title2 = (props) => (
   <h2>{props.text}</h2>
 )
 
@@ -15,7 +15,13 @@ const Button = (props) => (
 
 const Display = (props) => (
   <p>{props.text} {props.value}</p>
+ )
+
+
+const Positive = (props) => (
+  <p>{props.text} {props.value}  % </p>
 )
+
 
 
 function App() {
@@ -24,34 +30,62 @@ function App() {
    const [good, setGood] = useState(0)
    const [neutral, setNeutral] = useState(0)
    const [bad, setBad] = useState(0)
+   const [allFeedback, setAllFeedback] = useState(0)
+   const [average, setAverage] = useState(0)
+   const [positive, setPositive] = useState(0)
 
-  const setToGood = () => {
-    console.log('value now', good)
-    setGood(good + 1)
+  const setToGood = () =>{
+    setGood(good+1)
   }
 
-  const setToNeutral = () => {
-    console.log('value now', neutral)
-    setNeutral(neutral + 1)
+  const SetToNeutral = () => {
+    setNeutral(neutral+1)
   }
 
   const setToBad = () => {
-    console.log('value now', bad)
-    setBad(bad +1)
+    setBad(bad+1)
   }
+
+  setTimeout(() => {
+    setAllFeedback(good+neutral+bad)
+    
+  }, 100);
+
+  setTimeout(() => {
+    let g=good
+    let b=bad
+    while(g>0 && b>0){
+      g=g-1
+      b=b-1
+
+    }
+    if(g>0){
+      setAverage(g/allFeedback)
+    }
+  }, 100);
+
+  setTimeout(() => {
+    let h = (good/allFeedback)*100
+    console.log('positive', h)
+    if(h>=0){
+      setPositive((good/allFeedback)*100)
+    }
+    
+  }, 100);
 
   return (
     <div>
-      <H1 text={'give feedback'} />
-      <br />
-      <Button handleClick={() => setToGood()} text={'Good'} />
-      <Button handleClick={() => setToNeutral()} text={'Neutral'} />
-      <Button handleClick={() =>setToBad()} text={'Bad'} />
-      <br />
-      <H2 text={'statistics'} />
-      <Display text='good' value={good} />
-      <Display text='neutral' value={neutral} />
-      <Display text='bad' value={bad} />      
+      <Title1 text={'give feedback'} />
+      <Button handleClick={setToGood} text={'Good'} />
+      <Button handleClick={SetToNeutral} text={'Neutral'} />
+      <Button handleClick={setToBad} text={'Bad'} />
+      <Title2 text={'statistics'} />
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />    
+      <Display text="all"  value={allFeedback}/>
+      <Display text="average" value={average}/>
+      <Positive text="positive" value={positive}/>
     </div>
 
   );
